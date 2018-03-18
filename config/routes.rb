3c699f3a5d7971a9660d44ae7cab4e229b494681
rhys5690 <rhys5690@gmail.com>
2018-03-18 12:57:49 +1100

    organised authentication code

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  resources :users, only: [:create]
-  resource :session, only: [:create]
 
-  resource :confirmation
+  namespace :auth do
+    resources :users, only: [:create]
+    resource :session, only: [:create]
+    resource :confirmation
+  end
 
-  post 'login' => 'sessions#create'
-  get 'login' => 'sessions#new'
-  delete 'logout' => 'sessions#destroy'
+  post 'login' => 'auth/sessions#create'
+  get 'login' => 'auth/sessions#new'
+  delete 'logout' => 'auth/sessions#destroy'
 
-  get 'register' => 'users#new'
+  get 'register' => 'auth/users#new'
   resources :posts do
     resources :comments
   end
