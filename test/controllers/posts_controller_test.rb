30b5aa66160b2b3b7b9067f6c0540ba61a8982df
rhys5690 <rhys5690@gmail.com>
2018-03-20 08:16:46 +1100

    added tests

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
-class PostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+class PostsControllerTest < ActionController::TestCase
+  test "index action should be success" do
+    get :index
+    assert_response :success
+  end
+
+  test "new should be success" do
+    get :new
+    assert_response :success
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "email should not be valid" do
+    user = User.new(email: 'notvalid')
+
+    assert_not user.valid?
+  end
+
+  test "email should be valid" do
+    user = User.new(email: "test@valid.com")
+
+    assert user.valid?
+  end
 end
