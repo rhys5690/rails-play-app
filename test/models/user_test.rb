bf7a68e8d9d0ce20bc1fc4394a6b4cae0d21e61e
rhys5690 <rhys5690@gmail.com>
2018-03-20 08:39:13 +1100

    added custom test suite

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,14 +2,25 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test "email should not be valid" do
-    user = User.new(email: 'notvalid')
+    user = User.new(email: 'notvalid',
+                    password: '12345678',
+                    password_confirmation: '12345678')
 
     assert_not user.valid?
   end
 
   test "email should be valid" do
-    user = User.new(email: "test@valid.com")
+    user = User.new(email: "test@valid.com",
+                    password: '12345678',
+                    password_confirmation: '12345678')
 
     assert user.valid?
+  end
+
+  test "user should have confirmation_token" do
+    user = User.create(email: "blah@blah.com", password: '12345678',
+                       password_confirmation: '12345678')
+
+    assert_not_nil user.confirmation_token
   end
 end
