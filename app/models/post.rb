85a8cd8ef2303825a9dbe300c1a183213e8ec627
rhys5690 <rhys5690@gmail.com>
2018-03-24 11:05:07 +1100

    created post status

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  enum status: [:draft, :published]
+
   has_many :comments
   has_many :taggings
   has_many :tags, through: :taggings
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320205143) do
+ActiveRecord::Schema.define(version: 20180323223922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20180320205143) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
   end
 
   create_table "taggings", force: :cascade do |t|
